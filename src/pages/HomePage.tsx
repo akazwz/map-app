6dc04c0c7a090536fceb384de939df34e6bd166a
zwz <akazwz@pm.me>
2021-12-15 23:52:42 +0800

    change style

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,30 +1,23 @@
-import { MapContainer,TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { LatLng } from 'leaflet'
 
 export default function HomePage() {
+  const position = [51.505, -0.09]
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        alignContent: 'center'
-      }}
-    >
-      <div
-        style={{
-          textAlign: 'center',
-          width: '100%',
-        }}
-      >
-      </div>
-      <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
-        scrollWheelZoom={false}
-      >
+    <div>
+      <MapContainer center={new LatLng(51.505, -0.09)} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <Marker position={new LatLng(51.505, -0.09)}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
       </MapContainer>
+      <div id="map" style={{ height: '60px', width: '60px' }} />
     </div>
   )
 }
